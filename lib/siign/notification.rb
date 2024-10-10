65f6d4233051b8d77c3f5f53327554d6717da9a9
François de Metz <francois@2metz.fr>
2024-10-10 15:44:57 +0200

    Fix and configure rubocop Methods/MethodLength.

--- lib/siign/notification.rb
+++ lib/siign/notification.rb
@@ -9,19 +9,25 @@ module Siign
       notification_url = ENV.fetch('NOTIFICATION_URL', nil)
       return if notification_url.nil?
 
-      msg = case status
-            when :signed
-              "Bonne nouvelle, devis #{quote_title} signé !"
-            when :refused
-              "Mauvaise nouvelle, devis #{quote_title} refusé !"
-            when :expired
-              "Sale nouvelle, devis #{quote_title} expiré !"
-            when :aborted
-              "Sale nouvelle, devis #{quote_title} annulé !"
-            end
+      msg = notification_msg(status, quote_title)
       return if msg.nil?
 
       Faraday.get(notification_url.gsub(/\${msg}/, ERB::Util.url_encode(msg)))
+    end
+
+    private
+
+    def notification_msg(status, quote_title)
+      case status
+      when :signed
+        "Bonne nouvelle, devis #{quote_title} signé !"
+      when :refused
+        "Mauvaise nouvelle, devis #{quote_title} refusé !"
+      when :expired
+        "Sale nouvelle, devis #{quote_title} expiré !"
+      when :aborted
+        "Sale nouvelle, devis #{quote_title} annulé !"
+      end
     end
   end
 end
